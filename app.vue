64650328efc80a910b91fe57f4988219b051e02a
Jon LinuxLaptop <jonmatthis@gmail.com>
2024-04-08 21:02:19 -0400

    cogmate bb!

--- app.vue
+++ app.vue
@@ -1,5 +1,31 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="app">
+    <!-- Your layout wrapper -->
+
+    <header>
+      <!-- Your header -->
+    </header>
+    <!-- Your page content (default is pages/index.vue) -->
+    <NuxtPage />
+    <footer>
+      <!-- Your footer -->
+    </footer>
   </div>
 </template>
+
+<script setup>
+// Any script you might need
+</script>
+
+<style scoped>
+
+#app{
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  width: 100vw;
+  max-height: 100vh;
+  margin: 0;
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ChatBox />
+  </div>
+</template>
+<script setup lang="ts">
+</script>
+
+<style scoped>
+
+#app{
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  width: 100vw;
+  max-height: 100vh;
+  margin: 0;
+}
+
+</style>
